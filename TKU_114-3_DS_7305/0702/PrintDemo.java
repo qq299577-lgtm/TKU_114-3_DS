018d0fe345865758aa6c4eaa83c62bfef60306d4
qq299577-lgtm <qq299577@gmail.com>
2026-07-04 23:01:39 +0800

    Add files via upload

--- TKU_114-3_DS_7305/0702/PrintDemo.java
+++ TKU_114-3_DS_7305/0702/PrintDemo.java
@@ -1,11 +1,10 @@
-public class ProfileCard {
+public class PrintDemo {
     public static void main(String[] args) {
         
-        String name = "Amy";
+        String name = "蔡竣宇";
         String department = "Information Management";
         String favoriteCourse = "Programming";
-
-    
+        
         System.out.println("--- Profile ===");
         System.out.println("Name: " + name);
         System.out.println("Department: " + department);
