2efc981a3a86d6355c61278ed05ec0df4e40f9df
qq299577-lgtm <qq299577@gmail.com>
2026-07-04 23:06:08 +0800

    Delete TKU_114-3_DS_7305 directory

--- TKU_114-3_DS_7305/0702/CommentDemo.java
+++
@@ -1,8 +0,0 @@
-public class CommentDemo {
-    public static void main(String[] args) {
-        System.out.println("Welcome");
-
-        System.out.println("Start learning Java.");
-        
-    }
-}
